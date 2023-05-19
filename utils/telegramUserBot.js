a835475301e98dd658b4d61a21db677c5869e01b
Konstantin Kupriyanov <0864380@gmail.com>
2023-05-19 21:31:25 +0200

    smol refactoring

--- utils/telegramUserBot.js
+++ utils/telegramUserBot.js
@@ -12,8 +12,6 @@ import { Pool } from './pool.js';
 const token = '6227440710:AAHX6WL8iob8IhCeL-7IiUJKS5GVl2Muow4';
 export const bot = new TelegramBot(token);
 
-await bot.setWebHook('https://poo-tracker.onrender.com:443/webhook');
-
 // if (bot.isPolling()) {
 //     console.log('Bot is polling');
 //     bot.stopPolling();
