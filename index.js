f5f47040af6bcfff6a7fa815ada65345bd4776b2
Konstantin Kupriyanov <0864380@gmail.com>
2023-05-20 13:18:08 +0200

    stupid typos as always

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ async function main() {
         app.listen(PORT, () => {
             console.log(`Server started on port ${PORT}`);
         });
-        // Set webhook
-        await bot.setWebHook('https://poo-tracker-test.onrender.com:443/webhook');
         // Activate bot
         const token = '6227440710:AAHX6WL8iob8IhCeL-7IiUJKS5GVl2Muow4';
         const bot = new TelegramBot(token);
+        // Set webhook
+        await bot.setWebHook('https://poo-tracker-test.onrender.com:443/webhook');
         // Handle webhook request
         app.post('/webhook', (req, res) => {
             const { message } = req;
