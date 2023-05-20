740abc2315449de9d133c6aab1c7a06e70e00596
Konstantin Kupriyanov <0864380@gmail.com>
2023-05-20 13:14:03 +0200

    debugging bot

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 import express from 'express';
-import { client } from './utils/db.js';
-import { bot } from './utils/telegramUserBot.js';
+// import { client } from './utils/db.js';
+// import { bot } from './utils/telegramUserBot.js';
 
 export const app = express();
 const PORT = process.env.PORT || 3030;
@@ -16,11 +16,13 @@ async function main() {
         });
         // Set webhook
         await bot.setWebHook('https://poo-tracker-test.onrender.com:443/webhook');
+        // Activate bot
+        const token = '6227440710:AAHX6WL8iob8IhCeL-7IiUJKS5GVl2Muow4';
+        const bot = new TelegramBot(token);
         // Handle webhook request
         app.post('/webhook', (req, res) => {
-            // const { message } = req;
-            // handleRequest(message);
-            console.dir('Message:', req);
+            const { message } = req;
+            console.log('Message:', message);
             res.sendStatus(200);
         });
     } catch (err) {
