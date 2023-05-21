f60d10b8edeec076da6305829b72ad269a35ba60
Konstantin Kupriyanov <0864380@gmail.com>
2023-05-21 21:49:04 +0200

    refactored

--- utils/telegramBot.js
+++ utils/telegramBot.js
@@ -1,13 +1,6 @@
-import TelegramBot from "node-telegram-bot-api";
-import { app } from "./../index.js";
 
-const token = '6227440710:AAHX6WL8iob8IhCeL-7IiUJKS5GVl2Muow4';
-export const bot = new TelegramBot(token);
 
-await bot.setWebHook('https://poo-tracker-test.onrender.com:443/webhook');
 
-app.post('/webhook', (req, res) => {
-    const { body } = req;
-    bot.sendMessage(body.chat.id, body);
-    res.sendStatus(200);
-});
+
+
+
