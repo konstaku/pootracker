4b23ea1ee2109154969e53eb70cef9fdf70b2a6d
Konstantin Kupriyanov <0864380@gmail.com>
2023-05-21 21:34:57 +0200

    typos

--- utils/telegramBot.js
+++ utils/telegramBot.js
@@ -1,5 +1,5 @@
 import TelegramBot from "node-telegram-bot-api";
-import { app } from "./../main.js";
+import { app } from "./../index.js";
 
 const token = '6227440710:AAHX6WL8iob8IhCeL-7IiUJKS5GVl2Muow4';
 export const bot = new TelegramBot(token);
