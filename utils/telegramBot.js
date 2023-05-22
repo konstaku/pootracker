c6d0a80df06935c4f7178dd10e8bc1428cea34c2
Konstantin Kupriyanov <0864380@gmail.com>
2023-05-22 17:39:17 +0200

    testing cases for main menu

--- utils/telegramBot.js
+++ utils/telegramBot.js
@@ -3,20 +3,19 @@ import { bot } from './../index.js';
 export class Dialogue {
     constructor(message) {
         this.chatId = message.chat.id;
-        this.currentStage = 'idle';
+        this.state = 'idle';
     }
 
     processMessage(message) {
         try {
             const command = message.text;
 
-            switch(this.currentStage) {
+            switch(this.state) {
             case 'idle':
-                if (command === '/go') {
-                    this.mainMenuChoice(message);
-                } else {
-                    bot.sendMessage(this.chatId, 'Type /go to see main menu');
-                }
+                this.showMainMenu(message);
+                break;
+            case 'onMainMenu':
+                this.handleMainMenuChoice(message);
                 break;
             default:
                 bot.sendMessage(this.chatId, 'Nothing here yet...');
@@ -26,12 +25,35 @@ export class Dialogue {
         }
     }
 
-    mainMenuChoice(mainMenuMessage) {
+    showMainMenu(idleMessage) {
+        if (idleMessage.text !== '/go') {
+            bot.sendMessage(this.chatId, 'Type /go to see main menu');
+            return;
+        }
+
         bot.sendMessage(this.chatId, 'What do you want?', {
             reply_markup: {
                 keyboard: [['Add pools'], ['Remove pools'], ['View pool stats']],
                 one_time_keyboard: true,
             },
         });
+
+        this.state = 'onMainMenu';
+    }
+
+    handleMainMenuChoice(mainMenuChoice) {
+        switch (mainMenuChoice.text.toLowerCase) {
+        case 'add pools':
+            bot.sendMessage(this.chatId, 'You selected to add pools');
+            break;
+        case 'remove pools':
+            bot.sendMessage(this.chatId, 'You selected to remove pools');
+            break;
+        case 'view pool stats':
+            bot.sendMessage(this.chatId, 'You selected to view pool stats');
+            break;
+        default:
+            bot.sendMessage(this.chatId, 'Select something normal');
+        }
     }
 }
