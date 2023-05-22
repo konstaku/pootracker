261cbbcf3e6ab64d2feb79a4b32d6a30c654584b
Konstantin Kupriyanov <0864380@gmail.com>
2023-05-22 12:38:09 +0200

    trying to organize classes to work with tg webhooks

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 import express from 'express';
 import TelegramBot from "node-telegram-bot-api";
+import { Dialogue } from './utils/telegramBot.js';
 
 const token = '6227440710:AAHX6WL8iob8IhCeL-7IiUJKS5GVl2Muow4';
 const bot = new TelegramBot(token);
@@ -8,6 +9,8 @@ const bot = new TelegramBot(token);
 const PORT = process.env.PORT || 3030;
 export const app = express();
 app.use(express.json());
+
+const dialogues = [];
 
 async function main() {
     try {
@@ -20,8 +23,17 @@ async function main() {
         // Handle webhook
         app.post('/webhook', (req, res) => {
             const { body } = req;
-            console.log('BODY:', body);
-            bot.sendMessage(body.message.chat.id, body.message.text);
+            const message = body.message;
+            let dialogue;
+
+            if (!dialogues.some(dialogue => dialogue.id === message.chat.id)) {
+                dialogue = new Dialogue(message);
+            } else {
+                dialogue = dialogues.find(dialogue => dialogue.id === message.chat.id);
+            }
+
+
+            bot.sendMessage(message.chat.id, `Dialogue: ${dialogue.id}\nStage: ${dialogue.currentStage}`);
             res.sendStatus(200);
         });
     } catch (err) {
--- utils/telegramBot.js
+++ utils/telegramBot.js
@@ -1,6 +1,17 @@
+import { bot } from './../index.js';
 
+class Dialogue {
+    constructor(message) {
+        this.chatId = message.chat.id;
+        this.currentStage = 'idle';
+    }
 
-
-
-
-
+    processMessage(command) {
+        switch(command) {
+        case '/go':
+            this.currentStage = 'onMainMenu';
+            bot.sendMessage(this.chatId, 'You are on the main menu');
+            break;
+        } 
+    }
+}
