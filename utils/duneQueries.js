6e8b55c1458895ddb968be03a3e142edf72b736b
Konstantin Kupriyanov <0864380@gmail.com>
2023-05-31 15:29:05 +0200

    typos

--- utils/duneQueries.js
+++ utils/duneQueries.js
@@ -1,4 +1,4 @@
-import queryList from './../data/queryList.json' assert { type: 'json' };
+// import queryList from './../data/queryList.json' assert { type: 'json' };
 
 export async function enrichPoolData(pool) {
     // This function fetches the pool from database, fetches additional data from Dune (pool fee)
